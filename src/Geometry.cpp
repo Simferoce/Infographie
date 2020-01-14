0017d30a68f042ae8b0cc2703f0f02ae4feac6fe
gabriel bouchard <gaboutch@outlook.com>
2020-01-14 17:37:04 -0500

    Update Geometry.cpp

--- src/Geometry.cpp
+++ src/Geometry.cpp
@@ -11,13 +11,6 @@ void Geometry::makeQuad(VAO &vao) {
     vert.push_back(.5f);vert.push_back(.5f);vert.push_back(0);
     vert.push_back(.5f);vert.push_back(-.5);vert.push_back(0);
 
-    auto indices = std::vector<unsigned int>();
-    indices.push_back(2);
-    indices.push_back(1);
-    indices.push_back(0);
-    indices.push_back(0);
-    indices.push_back(3);
-    indices.push_back(2);
     auto uvs = std::vector<float>();
     for (int i = 0;i < (vert.size()/ 3) / 4; ++i) {
         uvs.push_back(0);
@@ -29,6 +22,16 @@ void Geometry::makeQuad(VAO &vao) {
         uvs.push_back(1);
         uvs.push_back(1);
     }
+
+    auto indices = std::vector<unsigned int>();
+    indices.push_back(2);
+    indices.push_back(1);
+    indices.push_back(0);
+    indices.push_back(0);
+    indices.push_back(3);
+    indices.push_back(2);
+
+
     vao.indicies(indices.data(),indices.size());
     vao.put(0,3,vert.data(),vert.size());
     vao.put(1,2,uvs.data(),uvs.size());
