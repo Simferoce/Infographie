b436f614bef63f912f0413fa506cbaea5b9c3fd6
gabriel bouchard <gaboutch@outlook.com>
2020-01-14 14:19:47 -0500

    opening window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#define GLEW_STATIC
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //INITIALIZING GLFW
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+    std::cout <<"Opening window..." << std::endl;
+    GLFWwindow* window;
+    if (glfwInit())
+    {
+        std::cout <<"Initialized GLFW" << std::endl;
+         window= glfwCreateWindow(900,500, "title", NULL, NULL);
+        if (!window)
+        {
+            return 1;
+        }
+        glfwMakeContextCurrent(window);
+    }
+    //INITIALIZING GLEW
+    glewInit() == GLEW_OK ?
+    std::cout << "Initialized GLEW" << std::endl :
+    std::cerr << "FAILED:GLEW INITIALIZATION" << std::endl;
+
+    //MAIN LOOP
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        if(glfwGetKey(window,GLFW_KEY_ESCAPE))
+        {
+            glfwSetWindowShouldClose(window,true);
+        }
+        glfwSwapBuffers(window);
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+    //CLEANUP
+    glfwTerminate();
+    delete window;
     return 0;
 }
