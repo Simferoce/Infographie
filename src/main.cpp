dbd3864533e2cb5430f682e5a690bf14761d770c
gabriel bouchard <gaboutch@outlook.com>
2020-01-14 18:07:23 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ int main() {
     //INITIALIZING GLFW
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    //glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
     GLFWwindow *window;
     if (glfwInit()) {
         std::cout << "Initialized GLFW" << std::endl;
